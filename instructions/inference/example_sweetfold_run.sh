a95255d6244e6a0e8324e8a8d753d34f8ca1e4d8
Keshav Sundar <keshavsundar4@gmail.com>
2026-08-05 14:22:41 -0400

    Update example_sweetfold_run.sh

--- instructions/inference/example_sweetfold_run.sh
+++ instructions/inference/example_sweetfold_run.sh
@@ -5,9 +5,13 @@
 source activate /work/keshavsundar/env/sweetfold
 
 # Run Boltz prediction
-boltz predict glycan_test.yaml \
+boltz predict example.yaml \
   --cache /work/keshavsundar/env/sweetfold/weights \
   --checkpoint /work/keshavsundar/env/sweetfold/weights/boltz1_glycan_epoch_23.ckpt \
   --no_potentials \
   --sampling_steps 200 \
   --diffusion_samples 10
+
+# Checkpoint refers to the checkpoint that the Boltz is being ran on. 
+#Potentials (physical steering) from Boltz is turned of. This script predicts 10 diffusion samples
+# It runs for 200 diffusion steps
